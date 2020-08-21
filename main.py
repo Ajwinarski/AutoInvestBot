1d03830d235127db6bcd1cc0e6209273285ddc41
Keenan McNamara <keenan.mcnamara12@gmail.com>
2020-08-20 22:33:04 -0500

    Outline of some classes, we can get rid of if we don't want them

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import alpaca_trade_api as tradeapi
 from alpaca_trade_api import StreamConn
-import threading
-import time
-import datetime
-
 from config import *
+from enum import Enum, unique
+import time
+
+@unique
+class Action(Enum):
+    Buy = 1
+    Self = 2
+
+class AnalysisResult:
+    def __init__(self, ticker: str, action: Action, amount: int):
+        self.ticker = ticker
+        self.action = action
+        self.amount = amount
+
+class Analyzer: 
+    # Hard coding for now, but look through current holdlings to determine if should sell.
+    # If balance available (within some threshold of account value) look for additional stocks to purchase
+    def analyze_result(self) -> AnalysisResult:
+        return AnalysisResult("SPY", Action.Buy, 100)
+
+class Trader:
+    def trade_result(self, AnalysisResult) -> str:
+        return "hi"
 
 class AutoInvestBot:
-  def __init__(self):
-    self.alpaca = tradeapi.REST(API_KEY, API_SECRET_KEY, ALPACA_BASE_ENDPOINT)
+    def __init__(self):
+        self.api = tradeapi.REST(API_KEY, API_SECRET_KEY, ALPACA_BASE_ENDPOINT)
 
-  # Obtain and print account information
-  def account_info(self):
-    account = self.alpaca.get_account()
-    print(account)
-    
-  # Used to submit buy or sell orders
-  def order(self, side, stock, qty):
-    # Check that qty is greater than 0
-    if(qty > 0):
-      try:
-        self.alpaca.submit_order(stock, qty, side, "market", "day")
-        print("Purchased " + str(qty) + " shares of " + stock + ".")
-      except:
-        print("ERROR: Purchase order for " + str(qty) + " shares of " + stock + " did not go through.")
-        resp.append(False)
+    # Obtain and print account information
+    def account_info(self):
+        account = self.api.get_account()
+        print(account)
+
+    def worker_thread(self):
+        analyzer = Analyzer()
+        trader = Trader()
+        while True:
+            analysis_result = analyzer.analyze_result()
+            trade_result = trader.trade_result(analysis_result)
+            print(trade_result)
+            time.sleep(10)
 
 if __name__ == "__main__":
-  bot = AutoInvestBot()
-  
-  bot.account_info()
+    bot = AutoInvestBot()
+    
+    bot.account_info()
+
+    bot.worker_thread()
+    
